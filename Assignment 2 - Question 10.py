5bca4010c226f7e0096d25f5297cfa45176a531a
DualDustin <dualdustin@gmail.com>
2026-02-10 22:52:30 -0700

    Finish logic of question 10 getting line numbers that keyword is found

--- Assignment 2 - Question 10.py
+++ Assignment 2 - Question 10.py
@@ -6,3 +6,47 @@
 # 
 ###
 
+def find_lines_containing(filename, keyword):
+    numberOfLinesToShow = 3
+
+
+    # first check if file exists
+    try:
+        with open(filename, "r") as textFileInput:
+            # print("Ready to go")
+            textFileInput.close()
+        textFileInput.close()
+    except FileNotFoundError:
+        print("File Name provided was not valid (found)")
+        return "Error"
+    
+
+    # open file
+    with open(filename, "r") as textFileInput:
+
+        # go through each line
+        # checking if keyword is contained
+        # if so add line number into a list
+        currentTextFileLineNumber = 1
+        listOfLineNumbersThatContainKeyword = []
+        for singularLineInFile in textFileInput:
+
+            if keyword in singularLineInFile:
+                listOfLineNumbersThatContainKeyword.append(currentTextFileLineNumber)
+
+            currentTextFileLineNumber += 1
+
+
+        # print("Number of lines: " + str(currentTextFileLineNumber))
+        # print(listOfLineNumbersThatContainKeyword)
+
+
+        
+
+
+
+
+
+
+
+find_lines_containing("sample-file.txt", "data")
